e746480a091621c546cb9efd5b3ff905e038d710
Ah-uuu <z033910751@gmail.com>
2025-02-26 20:29:07 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <BookingForm />
+  </div>
+</template>
+
+<script>
+import BookingForm from "./components/BookingForm.vue";
+
+export default {
+  components: {
+    BookingForm,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  background: #f8f9fa;
+  padding: 20px;
+}
+</style>
